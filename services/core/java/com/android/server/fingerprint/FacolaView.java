90fec57c4a8778d93ff77fa065a88ef636b16fe9
penn5 <penn5@users.noreply.github.com>
2019-06-24 20:18:03 +0100

    Facola to not dim background during AOD
    not sure if this helps tho

--- services/core/java/com/android/server/fingerprint/FacolaView.java
+++ services/core/java/com/android/server/fingerprint/FacolaView.java
@@ -27,6 +27,7 @@ import android.view.MotionEvent;
 import android.util.Slog;
 
 import android.view.WindowManager;
+import android.view.Display;
 import android.graphics.PixelFormat;
 import android.view.Gravity;
 import android.os.Handler;
@@ -53,6 +54,7 @@ public class FacolaView extends ImageView implements OnTouchListener {
     private boolean visible = false;
 
     private final WindowManager mWM;
+    private final Display mDisplay;
     FacolaView(Context context) {
         super(context);
 
@@ -79,6 +81,7 @@ public class FacolaView extends ImageView implements OnTouchListener {
         mPaintShow.setColor(Color.argb(0x18, 0x00, 0xff, 0x00));
         setOnTouchListener(this);
         mWM = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = mWM.getDefaultDisplay();
         Slog.d("PHH-Enroll", "Created facola...");
         try {
             if(mW != -1)
@@ -147,8 +150,12 @@ public class FacolaView extends ImageView implements OnTouchListener {
             mWM.updateViewLayout(this, mParams);
             return false;
         }
-
-        mParams.dimAmount = TOUCHED_DIM;
+        // Don't dim AOD or other weird states
+        if (mDisplay.getState() == Display.STATE_ON) {
+            mParams.dimAmount = TOUCHED_DIM;
+        } else {
+            mParams.dimAmount = UNTOUCHED_DIM;
+        }
         mParams.screenBrightness = 1.0f;
         mWM.updateViewLayout(this, mParams);
 
